1b0b264a206072cef4817e5e7bb698cd2b2d801a
yEsPaP <h.sachmanovas@gmail.com>
2019-07-10 12:43:09 +0300

    config

--- user/busybox/include/autoconf.h
+++ user/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.12.1
  */
-#define AUTOCONF_TIMESTAMP "2019-07-09 16:58:04 EEST"
+#define AUTOCONF_TIMESTAMP "2019-07-10 12:42:50 EEST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
